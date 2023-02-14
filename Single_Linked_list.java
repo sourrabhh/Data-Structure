b65f83ff2f2c445d2fdab58de374798f8dea6519
sourrabhh <121559280+sourrabhh@users.noreply.github.com>
2023-02-14 11:10:33 +0530

    Add files via upload

---
+++ Single_Linked_list.java
@@ -0,0 +1,81 @@
+
+
+class Single_Linked_List
+{
+    
+    public static void main(String[] args) 
+    {
+        Single_Linked_List ll = new Single_Linked_List();
+
+        ll.addData(1);
+        ll.addData(2);
+        ll.addData(3);
+        ll.addData(4);
+        ll.addData(5);
+
+        ll.display();
+        System.out.println("\n");
+        ll.insertFirst(20);
+        System.out.println("After the Insertion \n");
+
+        ll.display();
+
+    }
+
+    private class node
+    {
+        private int data;
+        private node next;
+
+        public node(int data)   // Constructor
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Head & Tail Initialization 
+    public node head = null;
+    public node tail = null;
+
+    public void insertFirst(int value)
+    {
+        node first = new node(value);
+        first.next = head;
+        head = first;
+    }
+
+    public void addData(int data)
+    {
+        node newnode = new node(data);  // Point to the class node for initialization
+
+        if(head == null)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
+
+    public void display()
+    {
+        node current = head;
+
+        if(head == null)
+        {
+            System.out.println("Empty Linked List ");
+            return;
+        }
+        
+        while(current != null)
+        {
+            System.out.print(current.data +" ");
+            current = current.next;
+        }
+    }
+
+}
